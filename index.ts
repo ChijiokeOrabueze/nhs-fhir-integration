479f41f57ebae3b3ca08cabba1a06dba2adc2442
Chijioke Orabueze <valentineorabueze@gmail.com>
2023-02-19 14:47:09 +0100

    feature: generated and stored private and public keys for signing and verifying jwt

--- index.ts
+++ index.ts
@@ -9,6 +9,14 @@ const app: Application = express();
 
 app.use(express.json());
 
+const appId = "a3e29d55-3f7a-452e-b484-bf9fd8c8c932";
+const apiKey = "0SIPQYEnd3kz620kGpYAkvVeZaG7iupO";
+const apiSecret = "wperGRy6oQFVvOHX";
+
+const generateAndSignJwt = () => {
+
+    
+}
 app.get("/", (req, res) => {
     console.log("hi")
 })
